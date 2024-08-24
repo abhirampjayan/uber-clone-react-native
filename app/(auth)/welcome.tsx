0cdffac081ba16d2c2698364b4208c9733671226
abhirampjayan <abhiramp08@gmail.com>
2024-08-24 13:30:44 +0530

    added types

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -7,24 +7,24 @@ const Welcome = () => {
   const swiperRef = useRef<Swiper>(null);
   const [active, setActive] = useState(0);
   return (
-    <SafeAreaView>
+    <SafeAreaView className="min-h-full max-h-full">
       <View className="w-full flex flex-row justify-end">
         <TouchableOpacity
           onPress={() => {
             router.replace("/(auth)/sign-up");
           }}
-          className="flex bg-red-300 items-end justify-center p-5"
+          className="flex items-end justify-center p-5"
         >
           <Text>Skip</Text>
         </TouchableOpacity>
       </View>
-      <View className="w-full bg-amber-100 flex flex-row grow">
+      <View className="h-full flex pb-12">
         <Swiper
           loop={false}
           ref={swiperRef}
           onIndexChanged={(index) => setActive(index)}
-          dot={<View className="w-2 h-2 bg-amber-500 rounded-full m-1" />}
-          activeDot={<View className="w-2 h-2 bg-amber-800 rounded-full m-1" />}
+          dot={<View className="w-2 h-2 bg-gray-300 rounded-full m-1" />}
+          activeDot={<View className="w-2 h-2 bg-blue-500 rounded-full m-1" />}
         >
           <View>
             <Text>hello</Text>
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const welcome = [
+  {
+    name: "hwllo",
+    type: "Text",
+    Image: "",
+  },
+];
