18925b5666cdcd2728ceba469b6c3c1545de510d
abhirampjayan <abhiramp08@gmail.com>
2024-09-13 23:11:40 +0530

    added temporary homepage for testing

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,23 @@
-import { SafeAreaView, Text } from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
-const Home = () => {
+export default function Page() {
+  const { user } = useUser();
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign In</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign Up</Text>
+        </Link>
+      </SignedOut>
+    </View>
   );
-};
-
-export default Home;
+}
