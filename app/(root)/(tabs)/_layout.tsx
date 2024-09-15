f45f8124b3c0a85bc50ded32b1d9ddb09ca4e378
abhirampjayan <abhiramp08@gmail.com>
2024-09-15 20:12:07 +0530

    Added Tabs

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,10 +1,72 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import TabIcon from "@/components/TabIcon";
+import { icons } from "@/constants";
 
 const Layout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="home" options={{ headerShown: true }} />
-    </Stack>
+    <Tabs
+      initialRouteName="home"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#333",
+          borderRadius: 50,
+          paddingBottom: 0,
+          overflow: "hidden",
+          marginBottom: 20,
+          marginHorizontal: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          alignItems: "center",
+          position: "absolute",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.home} alt="home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          headerShown: true,
+          title: "chat",
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.chat} alt="chat" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rides"
+        options={{
+          headerShown: true,
+          title: "rides",
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.list} alt="rides" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: true,
+          title: "profile",
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.profile} alt="profile" />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
